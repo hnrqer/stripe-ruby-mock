9bf21fffb981237868b8e450c51f8b231e36bd26
Henrique Sarmento <henrique.sarmento@clickfunnels.com>
2020-06-19 19:28:02 -0300

    Add prices as an attr_reader

--- lib/stripe_mock/instance.rb
+++ lib/stripe_mock/instance.rb
@@ -57,7 +57,7 @@ module StripeMock
     attr_reader :accounts, :balance, :balance_transactions, :bank_tokens, :charges, :coupons, :customers,
                 :disputes, :events, :invoices, :invoice_items, :orders, :payment_intents, :payment_methods,
                 :setup_intents, :plans, :recipients, :refunds, :transfers, :payouts, :subscriptions, :country_spec,
-                :subscriptions_items, :products, :tax_rates, :checkout_sessions
+                :subscriptions_items, :products, :prices, :tax_rates, :checkout_sessions
 
     attr_accessor :error_queue, :debug, :conversion_rate, :account_balance
 
